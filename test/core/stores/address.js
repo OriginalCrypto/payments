ec90ceb43a42ffb4f3cdd07e60f2d5f1d13233db
mishfit <mishochu@gmail.com>
2018-06-15 22:18:06 -0600

    working backwards from tests

---
+++ test/core/stores/address.js
@@ -0,0 +1,17 @@
+/*global describe */
+/*global it */
+
+const addressStore = require('../../../src/core/stores/address')
+
+const assert = require('assert')
+
+describe('stores.address', function() {
+  describe('#create()', function() {
+    it('should return -1 when the value is not present', function() {
+      const mnemonic = 'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat',
+            password = undefined,
+            address = addressStore.create(mnemonic, password)
+      assert.notEqual(undefined, address, 'address should not be undefined')
+    })
+  })
+})
