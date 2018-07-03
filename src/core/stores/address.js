fd1018bd9d011975211ae0c5ad4ef690c3458f04
Guitar Fire Yeah <matheson.lincoln12@hotmail.com>
2018-07-03 14:05:57 -0600

    passing test

--- src/core/stores/address.js
+++ src/core/stores/address.js
@@ -2,14 +2,23 @@ const bip39 = require('bip39'),
       hdkey = require('hdkey'),
       util = require('ethereumjs-util')
 
-function create (mnemonic, index, password) {
-    const path = ("m/44'/0'/0'/0/${index}", index)
-    const { privateToAddress } = util
+
+function create (mnemonic, password, index=0) {
+    
+    const path = "m/44'/0'/0'/0/"
+
+    try {
+     path += index.toString(10);
+
+     //add additional error checking
+    
+    } catch (error) {
+        return(-1)
+    }
 
     const address = hdkey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic, password)).derive(path).address
-    if (util.isValidAddress(address))
-    return address
-    else
+    if (util.isValidAddress(address)) return address;
+   
     return -1
 }
 
